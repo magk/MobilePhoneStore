2f2acef51f67bf9cfa8627e21d8a6798b0c378a1
DESKTOP-MTLSJG5\Win10 <bluevnd@gmail.com>
2021-12-24 17:19:20 +0700

    Add project files.

---
+++ MobilePhoneStore/Data/MvcBasicDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MobilePhoneStore.Models;
+
+namespace MobilePhoneStore.Data
+{
+    public class MvcBasicDbContext : DbContext
+    {
+        public MvcBasicDbContext(DbContextOptions<MvcBasicDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Phone> Phones { get; set; }
+    }
+}
---
+++ MobilePhoneStore/Models/Phone.cs
@@ -0,0 +1,8 @@
+﻿namespace MobilePhoneStore.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
